10502cb4de3116fa00c286466dc077cb74f1d5be
sauvageb <sauvageboris.pro@gmail.com>
2023-01-04 11:02:32 +0100

    security : create jwt utils and webconfig with filter jwt

---
+++ src/main/java/com/plancrazyappfrontofficespring/security/jwt/JwtResponse.java
@@ -0,0 +1,29 @@
+package com.plancrazyappfrontofficespring.security.jwt;
+
+public class JwtResponse {
+
+    private String userId;
+
+    private String token;
+
+    public JwtResponse(String userId, String token) {
+        this.userId = userId;
+        this.token = token;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
---
+++ src/main/java/com/plancrazyappfrontofficespring/security/jwt/JwtUtils.java
@@ -0,0 +1,63 @@
+package com.plancrazyappfrontofficespring.security.jwt;
+
+import com.plancrazyappfrontofficespring.model.AppUser;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Component
+public class JwtUtils {
+
+    @Value("${jwt.utils.secret}")
+    private String jwtSecret;
+    @Value("${jwt.utils.expiration.duration}")
+    private long expirationMs;
+
+    public String generateJwt(Authentication authentication) {
+        AppUser user = (AppUser) authentication.getPrincipal();
+
+        return Jwts.builder()
+                .setSubject(user.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(new Date().getTime() + this.expirationMs))
+                .signWith(Keys.hmacShaKeyFor(jwtSecret.getBytes(StandardCharsets.UTF_8)))
+                .compact();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(Keys.hmacShaKeyFor(jwtSecret.getBytes(StandardCharsets.UTF_8)))
+                    .build().parseClaimsJws(token);
+            return true;
+        } catch (MalformedJwtException e) {
+            System.err.println("Invalid JWT token: {} " + e.getMessage());
+        } catch (ExpiredJwtException e) {
+            System.err.println("JWT token is expired: {} " + e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            System.err.println("JWT token is unsupported: {} " + e.getMessage());
+        } catch (IllegalArgumentException e) {
+            System.err.println("JWT claims string is empty: {} " + e.getMessage());
+        }
+        return false;
+    }
+
+
+    public String getUsernameFromToken(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(Keys.hmacShaKeyFor(jwtSecret.getBytes(StandardCharsets.UTF_8)))
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+}
