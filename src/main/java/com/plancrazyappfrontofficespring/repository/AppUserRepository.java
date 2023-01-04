9875ae2e063dd571f8c08439efebc4d57d90df30
sauvageb <sauvageboris.pro@gmail.com>
2023-01-04 11:00:48 +0100

    userRepository : add two jpa method

--- src/main/java/com/plancrazyappfrontofficespring/repository/AppUserRepository.java
+++ src/main/java/com/plancrazyappfrontofficespring/repository/AppUserRepository.java
@@ -4,6 +4,13 @@ import com.plancrazyappfrontofficespring.model.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AppUserRepository  extends JpaRepository<AppUser, Long> {
+
+    boolean existsByEmail(String email);
+
+    Optional<AppUser> findUserByEmail(String email);
+
 }
